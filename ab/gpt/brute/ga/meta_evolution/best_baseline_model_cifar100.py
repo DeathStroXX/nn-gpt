4a598d86df89ae3c6d013e438903d46b40003af6
DeathStroXX <akashdepp.singh1110@gmail.com>
2026-07-26 19:57:21 +0200

    feat(meta_evolution): add generational diversity plot, stage cifar logs, update imagenet100 config and baseline metrics

--- ab/gpt/brute/ga/meta_evolution/best_baseline_model_cifar100.py
+++ ab/gpt/brute/ga/meta_evolution/best_baseline_model_cifar100.py
@@ -3,15 +3,15 @@ import torch.nn as nn
 from typing import List
 
 # --- HASH IDENTIFIERS (Ensures unique UUIDs for caching) ---
-# LR: 0.001
-# Momentum: 0.8
+# LR: 0.004
+# Momentum: 0.88
 # Activation: SiLU
-# Kernel: 3
+# Kernel: 5
 # Pooling: Max
-# Conv Type: Depthwise
+# Conv Type: Standard
 # Norm Type: BatchNorm
-# Optimizer: AdamW
-# FC Dropout: 0.1
+# Optimizer: SGD
+# FC Dropout: 0.15
 
 # --- MANDATORY FOR EVAL ENGINE ---
 def supported_hyperparameters():
@@ -19,7 +19,7 @@ def supported_hyperparameters():
 
 # --- Helper Classes ---
 class FractalDropPath(nn.Module):
-    def __init__(self, drop_prob: float = 0.0):
+    def __init__(self, drop_prob: float = 0.05):
         super().__init__()
         self.drop_prob = drop_prob
 
@@ -40,10 +40,7 @@ class FractalBlock(nn.Module):
         channels = int(channels)  
 
         activation_layer = nn.SiLU(inplace=True)
-        conv_layer = nn.Sequential(
-    nn.Conv2d(channels, channels, kernel_size=3, padding=1, groups=channels, bias=False),
-    nn.Conv2d(channels, channels, kernel_size=1, bias=False)
-)
+        conv_layer = nn.Conv2d(channels, channels, kernel_size=5, padding=2, bias=False)
         norm_layer = nn.BatchNorm2d(channels)
 
         # Assemble Convolutional Sequence
@@ -84,7 +81,7 @@ class FractalBackbone(nn.Module):
         total_blocks = int(4)
 
         for i in range(total_blocks):
-            blocks.append(FractalBlock(int(2), cur_chan, 0.0))
+            blocks.append(FractalBlock(int(2), cur_chan, 0.05))
             pools.append(nn.MaxPool2d(2))
 
             if i < total_blocks - 1:
@@ -140,7 +137,7 @@ class Net(nn.Module):
             dim_fused = self.features(dummy).shape[1]
         self.train()
 
-        self.fc_dropout = nn.Dropout(p=0.1)
+        self.fc_dropout = nn.Dropout(p=0.15)
         self.fc = nn.Linear(dim_fused, n_classes)
         self.to(device)
 
@@ -166,7 +163,7 @@ class Net(nn.Module):
 
     def train_setup(self, prm):
         self.criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
-        self.optimizer = torch.optim.AdamW(self.parameters(), lr=prm['lr'], betas=(prm['momentum'], 0.999), weight_decay=1e-4)
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=prm['lr'], momentum=prm['momentum'])
         self.max_batches = prm.get('max_batches', None)
 
         total_steps = 782 if self.max_batches is None else min(self.max_batches, 782)
